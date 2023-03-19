41a72214c92d7b4084abd88572a61a422717f86e
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-03-19 19:39:48 +0300

    change text for email message

--- src/domain/business-service.ts
+++ src/domain/business-service.ts
@@ -6,7 +6,10 @@ export const businessService = {
             from: "Blogs And Videos API", // sender address
             to: email, // list of receivers
             subject: "CONFIRMATION CODE", // Subject line
-            html: confirmationCode // html body
+            html: ` <h1>Thank for your registration</h1>
+       <p>To finish registration please follow the link below:
+          <a href='https://somesite.com/confirm-email?code=${confirmationCode}'>complete registration</a>
+      </p>`
         });
         return confirmationCode
     }
