fa7eea94d20b150a0efc666d244acbaa51eb67f4
fateevanastusha <anastasiafateeva2406@gmail.com>
2023-03-19 19:53:13 +0300

    debugging

--- src/domain/auth-service.ts
+++ src/domain/auth-service.ts
@@ -24,12 +24,8 @@ export const authService = {
         )
     },
     async checkForConfirmationCode (confirmationCode : string) : Promise <boolean>  {
-        const status: boolean = await authRepository.checkForConfirmationCode(confirmationCode)
-        if (!status) {
-            return false
-        } else {
-            return authRepository.changeConfirmatedStatus(confirmationCode)
-        }
+        return await authRepository.changeConfirmatedStatus(confirmationCode)
+
     },
     async updateConfirmationCode (confirmationCode : string, email : string) : Promise <boolean> {
         return authRepository.changeConfirmationCode(confirmationCode,email)
